7dcc980eb1b60745a871d1846393c88e4ff714b3
Huy Tran <tanhuy998@gmailc.om>
2018-06-25 02:13:17 +0700

    commit for logIn again

--- control/C_Login.php
+++ control/C_Login.php
@@ -12,16 +12,16 @@
         $_SESSION['user'] = $user;
 
         if ($_SESSION['user']->IsAnonymous()) {
-            // header('Location: http://localhost/final/control/C_Checkout.php?log-err=1');
-            // exit;
+            header('Location: http://localhost/final/control/C_Checkout.php?log-err=1');
+            exit;
         }
         else {
-            // header('Location: http://localhost/final/control/C_Checkout.php');
-            // exit;
+            header('Location: http://localhost/final/control/C_Checkout.php');
+            exit;
         }
     }
     else {
-        // header('Location: http://localhost/final/control/C_Checkout.php?log-err=1');
-        // exit;
+        header('Location: http://localhost/final/control/C_Checkout.php?log-err=1');
+        exit;
     }
 ?>
